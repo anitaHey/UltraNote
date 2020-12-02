3fbbc5e45f061639657fb54404700645dfc1f9be
anitaHey <jiaa.anita@gmail.com>
2020-12-03 00:24:47 +0800

    20201202 Add MessageController

---
+++ src/Controller/MessageController.java
@@ -0,0 +1,27 @@
+package Controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class MessageController {
+    private String message_str;
+
+    @FXML
+    Button exit, enter;
+    @FXML
+    Label message;
+
+    public MessageController(String input){
+        this.message_str = input;
+    }
+
+    @FXML
+    public void initialize() {
+
+    }
+
+    public String getMessage_str() {
+        return message_str;
+    }
+}
